74f5222fd2410957b4f42021f033ee20ffc63932
gokulr2105 <126942175+gokulr2105@users.noreply.github.com>
2023-03-06 02:11:03 -0800

    change the project

--- test.java
+++ test.java
@@ -1,5 +1,13 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Gokul");  
+         
+     int a=10;
+     int b=37;
+     int c=a+b;
+     String S="gokul";
+     System.out.println("enter the valus:" +c);
+     System.out.println("this my name:" +S);
+        
     }  
 }
